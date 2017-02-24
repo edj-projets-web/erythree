a7f4e840a663579d6901ed55891ab1e1d68dc533
marinejea <marine.jeannin@sciencespo.fr>
2017-02-24 17:16:22 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+var video = document.getElementById("video"),
+fraction = 0.8;
+
 function rotate(element, angle) {
 	var coord = $(element)[0].getBBox();
 	var position = Number(coord.x + coord.width/2) +' '+ Number(coord.y + coord.height/2);
@@ -7,30 +10,37 @@ function rotate(element, angle) {
 $.get('radio.svg', function(res) {
 	var angle = 0;
 	document.getElementById("radio").appendChild(res.documentElement);
+	player = document.getElementById('audio');
 
 	$('#radio #button-left').click(function(){
 		angle += 10;
 		rotate(this, angle);
+		player.play();
 	});
-
-	// $('#radio #button-right').click(function(){
-	// 	angle -= 10;
-	// 	rotate(this, angle);
-	// });
 })
 
 $(function () {
         $(window).scroll(function () {
-             if ($(this).scrollTop() > 200 ) {
+        	//Position du scroll + moitié de l'écran visible
+        	var scroll = $(this).scrollTop()+window.innerHeight/4;
+
+             if (scroll > $('#info1').offset().top ) {
                 $('#info1').removeClass('hid')
             } else {
                 $('#info1').addClass('hid')
             }
 
-            if ($(this).scrollTop() > 400 ) {
+            if (scroll > $('#info2').offset().top ) {
                 $('#info2').removeClass('hid')
             } else {
                 $('#info2').addClass('hid')
+            }
+
+            //Condition pour démarrer et mettre en pause la vidéo dès lors qu'elle n'est plus visible
+            if (scroll > $('#video').offset().top && scroll < $('#video').offset().top+$('#video').height() ) {
+                video.play();
+            } else {
+                video.pause();
             }
     });
 })
