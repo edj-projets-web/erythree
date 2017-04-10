22a1807a296e667d88d9ce658c0aec1cb109459d
marionbir <marion.biremon@gmail.com>
2017-04-10 16:13:39 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -25,28 +25,17 @@ $.get($(radio).data('image'), function(res) {
 $(function() {
     $(window).scroll(function() {
         //Position du scroll + moitié de l'écran visible
-        var scroll = $(this).scrollTop() + window.innerHeight / 4;
+        var scroll = $(this).scrollTop() + window.innerHeight * 0.8;
 
-        if (scroll > $('#info1').offset().top) {
-            $('#info1').removeClass('hid')
-        } else {
-            $('#info1').addClass('hid')
-        }
-
-        if (scroll > $('#info2').offset().top) {
-            $('#info2').removeClass('hid')
-        } else {
-            $('#info2').addClass('hid')
-        }
+        $('.info').each(function() {
+            if (scroll > $(this).offset().top) {
+                $(this).removeClass('hid')
+            } else {
+                $(this).addClass('hid')
+            }
+        });
 
         $(".footer").toggle($(this).scrollTop() > 280);
-
-        //Condition pour démarrer et mettre en pause la vidéo dès lors qu'elle n'est plus visible
-        if (scroll > $('#video').offset().top && scroll < $('#video').offset().top + $('#video').height()) {
-            video.play();
-        } else {
-            video.pause();
-        }
     });
 
     $(".smooth-scroll").click(function(event) {
